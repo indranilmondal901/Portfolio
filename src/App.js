3807d99b0d36a666d1cb794edaf25e0b1192aee9
indranilmondal901 <122368872+indranilmondal901@users.noreply.github.com>
2024-06-27 23:02:26 +0530

    minor update

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Joyride from 'react-joyride';
-import HomeSection from './components/HomeSection';
-import About from './components/About';
-import Expertise from './components/Expertise';
-import Projects from './components/Projects';
-import Testimonial from './components/Testimonial';
-import ContactForm from './components/ContactForm';
-import Map from './components/Map';
-import Copyright from './components/Copyright';
-import Navbar from './components/Navbar';
-import ProfessionalExperience from './components/ProfessionalExperience';
-import KeyRoles from './components/KeyRoles';
-
-import ProfessionalExp from "./components/ProfessionalExp.json";
-
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Joyride from "react-joyride";
+import HomeSection from "./components/HomeSection";
+import About from "./components/About";
+import Expertise from "./components/Expertise";
+import Projects from "./components/Projects";
+import Testimonial from "./components/Testimonial";
+import ContactForm from "./components/ContactForm";
+import Map from "./components/Map";
+import Copyright from "./components/Copyright";
+import Navbar from "./components/Navbar";
+import ProfessionalExperience from "./components/ProfessionalExperience";
+import KeyRoles from "./components/KeyRoles";
 
 const App = () => {
   const [run, setRun] = useState(false);
   const [steps] = useState([
     {
-      target: '#home',
-      content: 'Welcome to my portfolio. This is the Home section where you can get an overview of who I am and what I do.',
-      placement: 'center',
+      target: "#home",
+      content:
+        "Welcome to my portfolio. This is the Home section where you can get an overview of who I am and what I do.",
+      placement: "center",
     },
     {
-      target: '#about',
-      content: 'In the About section, you can learn more about my background and experience as a Full Stack Web Developer.',
-      placement: 'top',
+      target: "#about",
+      content:
+        "In the About section, you can learn more about my background and experience as a Full Stack Web Developer.",
+      placement: "top",
     },
     {
-      target: '#expertise',
-      content: 'Here in the Expertise section, I outline my key skills and areas of specialization.',
-      placement: 'top',
+      target: "#expertise",
+      content:
+        "Here in the Expertise section, I outline my key skills and areas of specialization.",
+      placement: "top",
     },
     {
-      target: '#project',
-      content: 'This section showcases my projects, demonstrating the practical application of my skills.',
-      placement: 'top',
+      target: "#project",
+      content:
+        "This section showcases my projects, demonstrating the practical application of my skills.",
+      placement: "top",
     },
     {
-      target: '#testimonial',
-      content: 'Read testimonials from clients and colleagues who have worked with me.',
-      placement: 'top',
+      target: "#testimonial",
+      content:
+        "Read testimonials from clients and colleagues who have worked with me.",
+      placement: "top",
     },
     {
-      target: '#contact',
-      content: 'Feel free to reach out to me through the Contact section.',
-      placement: 'top',
+      target: "#contact",
+      content: "Feel free to reach out to me through the Contact section.",
+      placement: "top",
     },
   ]);
-
-  // useEffect(() => {
-  //   setRun(true);
-  // }, []);
 
   return (
     <BrowserRouter>
@@ -88,7 +86,10 @@ const App = () => {
             </>
           }
         />
-        <Route path="/professional-experience" element={<ProfessionalExperience data={ProfessionalExp}/>} />
+        <Route
+          path="/professional-experience"
+          element={<ProfessionalExperience />}
+        />
         <Route path="/key-roles" element={<KeyRoles />} />
       </Routes>
     </BrowserRouter>
@@ -96,5 +97,3 @@ const App = () => {
 };
 
 export default App;
-
-
